ab9312090b32373c04b4061fb94b9bf3fe31d119
Anur00p <with.anuroop90@gmail.com>
2013-02-07 21:33:50 +0530

    last commit

--- app1/app/controllers/posts_controller.rb
+++ app1/app/controllers/posts_controller.rb
@@ -11,29 +11,34 @@ def create
 end
 
 def show
-  user =User.find(params[:id])
-  @posts=user.posts.find(:all)
+  @user = User.find(params[:user_id])
+ @post= @user.posts.find(params[:id])
+end
+
+def index
+  @user =User.find(params[:user_id])
+  @posts=@user.posts.find(:all)
   
 end
 
 def edit 
- user=User.find(params[:id])
- @posts=user.posts.find(params[:user_id])
+ @user=User.find(params[:user_id])
+ @post=@user.posts.find(params[:id])
 end
 
 def update
-  user=User.find(params[:id])
-  @post=user.posts.find(params[:user_id])
+  @user=User.find(params[:user_id])
+  @post=@user.posts.find(params[:id])
   @post.update_attributes(params[:post])
   @post.save!
-  redirect_to user_post_path(user)
+  redirect_to user_post_path(@user)
  end
 
 def destroy
- user=User.find(params[:id])
- post=user.posts.find(params[:user_id])
- post.destroy
- redirect_to user_post_path(user)
+@user = User.find(params[:user_id])
+ @post= @user.posts.find(params[:id])
+ @post.destroy
+ redirect_to user_posts_path(@user)
 end
 
 end
--- app1/app/models/user.rb
+++ app1/app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   validates :email,:uniqueness => true
   has_many :posts, :dependent => :destroy
 
+  before_validation :ensure_name_has_a_value
+ 
+  protected
+  def ensure_name_has_a_value
+    if user_name.nil?
+      self.user_name = email unless email.blank?
+    end
+  end
 
 before_create do |user|
     user.user_name = user.user_name.capitalize
