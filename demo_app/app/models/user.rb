a3bd142a92fa797c0a90dfaf4d08adee16018757
Anur00p <with.anuroop90@gmail.com>
2013-02-28 12:51:23 +0530

    new app

---
+++ demo_app/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+ 
+#has_many :posts,:dependent => :destroy
+attr_accessible :name, :email
+#validates :email, :presence => true
+# validates :email,:uniqueness => true
+#validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]+\z/i
+#before_create do |user|
+#user.user_name = user.user_name.capitalize
+#end
+#before_update do |user|
+#user.user_name = user.user_name.capitalize
+#end
+
+end
+
---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '418e0c6585017337b147c4017c057ea2adac131dae597043772c54f53335927b214ad118bfb69519620877c3fba7312fe3d004dc13108117e039303f3e468191'
