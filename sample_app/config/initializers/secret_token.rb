79d8e053c3f5bf2eeafe77a6f83d8ad84a61a6f7
Anur00p <with.anuroop90@gmail.com>
2013-03-03 17:56:43 +0530

    finished sattic pages

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6d351c0cd4ef5da6fe4a0f1aa73c4db7232b75498e45a8f4a40df459e0e61a039ab11f0f6bbcd627b61d098a1fb52dd2e150e133c0423278d21ca543736e4ae7'
